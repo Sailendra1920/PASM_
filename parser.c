d38fbfc10cd379d29f9a7fec6377932b05b505a6
Sailendra1920 <sailendragautam1920@gmail.com>
2024-03-13 19:48:59 +0530

    Parser Update

--- parser.c
+++ parser.c
@@ -2,43 +2,14 @@
 
 #include "loader.h"
 
+#define NO_OF_OPCODES 17
 
-bool isOpcode(char * s){
-  if(strcasecmp(s,"MOV")==0)
-  return true;
-  else if(strcasecmp(s,"HLT")==0)
-  return true;
-  else if(strcasecmp(s,"ADD")==0)
-  return true;
-  else if(strcasecmp(s,"SUB")==0)
-  return true;
-  else if(strcasecmp(s,"MUL")==0)
-  return true;
-  else if(strcasecmp(s,"DIV")==0)
-  return true;
-  else if(strcasecmp(s,"MOD")==0)
-  return true;
-  else if(strcasecmp(s,"CALL")==0)
-  return true;
-  else if(strcasecmp(s,"RET")==0)
-  return true ; 
-  else if(strcasecmp(s,"CMP")==0)
-  return true;
-  else if(strcasecmp(s,"JE")==0)
-  return true ; 
-  else if(strcasecmp(s,"JNE")==0)
-  return true;
-  else if(strcasecmp(s,"JGE")==0)
-  return true;
-  else if(strcasecmp(s,"JLE")==0)
-  return true;
-  else if(strcasecmp(s,"PUSH")==0)
-  return true;
-  else if(strcasecmp(s,"POP")==0)
-  return true;
-  else if(strcasecmp(s,"PRINT")==0)
-  return true;
-  else
+bool isOpcode(char * s, char **table, int len){
+ for (int i = 0 ; i < len ; i++){
+    if(!strcasecmp(s,table[i])){
+      return true;
+    }
+  }
   return false;
 }
 
@@ -84,7 +55,7 @@ bool isLabel(char * s){
 }
 
 
-Token_type checker(char *s){
+Token_type checker(char *s, char **table){
 
   if(isReg(s))
   return REG;
@@ -94,20 +65,52 @@ Token_type checker(char *s){
   return DIR;
   else if(isLabel(s))
   return LAB;
-  else if(isOpcode(s))
+  else if(isOpcode(s,table, NO_OF_OPCODES))
   return OPCODE;
   else
   return DATA;
 }
+
+void init_optable(char **s)
+{
+    //says that the double pointer should point to char pointers
+    s  = calloc(NO_OF_OPCODES,sizeof(char*));
+  
+  for(int i = 0 ; i < NO_OF_OPCODES ; i++){
+    //allocates mem for the char 
+    s[i] = calloc(32,sizeof(char));
+  }
+  
+  s[0]= "MOV";
+  s[1]="HLT";
+  s[2]="PRINT";
+  s[3]="POP";
+  s[4]="PUSH";
+  s[5]="ADD";
+  s[6]="SUB";
+  s[7]="MUL";
+  s[8]="DIV";
+  s[9]="CALL";
+  s[10]="JE";
+  s[11]="MOD";
+  s[12]="RET";
+  s[13]="CMP";
+  s[14]="JNE";
+  s[15]="JGE";
+  s[16]="JLE";
+}
+
 
 Node * parsing( Node *head){
   
   Node *node = (Node *) malloc(sizeof(Node));
   node=head;
   
+  char **opcode_table;
+  init_optable(opcode_table);
   while(node->next != NULL){
     printf("token name: %s\n",node->token.name);
-    node->token.type = checker(node->token.name);
+    node->token.type = checker(node->token.name, opcode_table);
     printf("Type of the token: %d \n", node->token.type);
     node=node->next;
   }
